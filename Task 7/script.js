feb13f6b8e1a08ff34f480c02263d46a997d3e80
Paulius19Ka <kallabow2@gmail.com>
2025-03-05 11:57:23 +0200

    Added display of team names and player full names in HTML

--- Task 7/script.js
+++ Task 7/script.js
@@ -3,6 +3,24 @@ const ENDPOINT = 'NBA.json';
 fetch(ENDPOINT)
     .then(res => res.json())
     .then((data) => {
-    console.log(data);
-    console.log(data.teams[0]);
+    const output = document.querySelector('#output');
+    if (!output) {
+        throw new Error('The element widh id output does not exist in the html file');
+    }
+    data.teams.forEach((team) => {
+        const teamCard = document.createElement('div');
+        teamCard.classList.add('teamCard');
+        const teamName = document.createElement('h3');
+        teamName.textContent = team.name;
+        teamCard.appendChild(teamName);
+        team.players.forEach(player => {
+            const playerCard = document.createElement('div');
+            playerCard.classList.add('playerCard');
+            const playerName = document.createElement('span');
+            playerName.textContent = player.firstName.concat(' ', player.lastName);
+            playerCard.append(playerName);
+            teamCard.appendChild(playerCard);
+        });
+        output.appendChild(teamCard);
+    });
 });
--- Task 7/script.ts
+++ Task 7/script.ts
@@ -24,6 +24,29 @@ const ENDPOINT: string = 'NBA.json';
 fetch(ENDPOINT)
 .then(res => res.json())
 .then((data: NBA) => {
-    console.log(data);
-    console.log(data.teams[0]);
+    // console.log(data);
+    // console.log(data.teams[0]);
+    const output: HTMLDivElement | null = document.querySelector('#output');
+
+    if(!output){
+        throw new Error('The element widh id output does not exist in the html file');
+    }
+
+    data.teams.forEach((team) => {
+        const teamCard = document.createElement('div');
+        teamCard.classList.add('teamCard');
+        const teamName = document.createElement('h3');
+        teamName.textContent = team.name;
+        teamCard.appendChild(teamName);
+        team.players.forEach(player => {
+            const playerCard = document.createElement('div');
+            playerCard.classList.add('playerCard');
+
+            const playerName = document.createElement('span');
+            playerName.textContent = player.firstName.concat(' ', player.lastName);
+            playerCard.append(playerName);
+            teamCard.appendChild(playerCard)
+        })
+        output.appendChild(teamCard);
+    })
 });
