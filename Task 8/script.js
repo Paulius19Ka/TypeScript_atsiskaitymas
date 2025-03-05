9aba8705c07184a2fe7dd29ed015d175286b3ffa
Paulius19Ka <kallabow2@gmail.com>
2025-03-05 13:59:04 +0200

    Finished the functionality

--- Task 8/script.js
+++ Task 8/script.js
@@ -2,36 +2,38 @@
 var _a;
 const form = document.querySelector('#converterForm');
 if (!form) {
-    throw new Error("Element with id '#converterForm' does not exist in the HTML file");
+    throw new Error("Element with id '#converterForm' does not exist in HTML");
 }
 const submitBtn = document.createElement('input');
 submitBtn.setAttribute('type', 'submit');
 submitBtn.setAttribute('value', 'Submit');
 const metersInput = document.querySelector('#meter');
 if (!metersInput) {
-    throw new Error("Element with id 'meter' does not exist in the HTML file");
+    throw new Error("Element with id '#meter' does not exist in HTML");
 }
 const convertedUnits = document.createElement('div');
+const heading = document.createElement('h3');
+const cmSpan = document.createElement('span');
+const inSpan = document.createElement('span');
+const ftSpan = document.createElement('span');
+const miSpan = document.createElement('span');
+const ydSpan = document.createElement('span');
+convertedUnits.append(heading, cmSpan, inSpan, ftSpan, miSpan, ydSpan);
 (_a = form.nextElementSibling) === null || _a === void 0 ? void 0 : _a.appendChild(convertedUnits);
 form.appendChild(submitBtn);
 form.addEventListener('submit', e => {
     e.preventDefault();
     const meters = metersInput.valueAsNumber;
-    console.log(meters);
-    const centimeters = meters * 100;
-    const cmSpan = document.createElement('span');
-    cmSpan.textContent = String(centimeters);
-    const inches = meters * 39.37;
-    const inSpan = document.createElement('span');
-    inSpan.textContent = String(inches);
-    const feet = meters * 3.281;
-    const ftSpan = document.createElement('span');
-    ftSpan.textContent = String(feet);
-    const miles = meters / 1609;
-    const miSpan = document.createElement('span');
-    miSpan.textContent = String(miles);
-    const yards = meters * 1.094;
-    const ydSpan = document.createElement('span');
-    ydSpan.textContent = String(yards);
-    convertedUnits.append(cmSpan, inSpan, ftSpan, miSpan, ydSpan);
+    heading.textContent = `${meters}m converted to:`;
+    const centimeters = (meters * 100).toFixed(2);
+    cmSpan.textContent = `Centimeters:${centimeters}cm`;
+    const inches = (meters * 39.37).toFixed(2);
+    inSpan.textContent = `Inches:${inches}in`;
+    const feet = (meters * 3.281).toFixed(2);
+    ftSpan.textContent = `Feet:${feet}ft`;
+    const miles = (meters / 1609).toFixed(2);
+    miSpan.textContent = `Miles:${miles}mi`;
+    const yards = (meters * 1.094).toFixed(2);
+    ydSpan.textContent = `Yards:${yards}yd`;
+    form.reset();
 });
--- Task 8/script.ts
+++ Task 8/script.ts
@@ -9,49 +9,67 @@ Parašykite TS kodą, kuris leis vartotojui įvesti ilgį metrais ir pamatyti jo
 Pastaba: Atvaizdavimas turi būti matomas su kiekviena įvestimi ir pateikiamas <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+// Select form element in HTML
 const form: HTMLFormElement | null = document.querySelector('#converterForm');
+// Print error message in case form does not exist in HTML
 if(!form){
-    throw new Error("Element with id '#converterForm' does not exist in the HTML file");
+    throw new Error("Element with id '#converterForm' does not exist in HTML");
 }
-
+// Create a submit button
 const submitBtn: HTMLInputElement = document.createElement('input');
 submitBtn.setAttribute('type', 'submit');
 submitBtn.setAttribute('value', 'Submit');
-
+// Select the number input field element in HTML
 const metersInput: HTMLInputElement | null = document.querySelector('#meter');
+// Print error message in case the input element does not exist in HTML
 if(!metersInput){
-    throw new Error("Element with id 'meter' does not exist in the HTML file");
+    throw new Error("Element with id '#meter' does not exist in HTML");
 }
 
-const convertedUnits = document.createElement('div');
-form.nextElementSibling?.appendChild(convertedUnits);
+// Create a div, a heading, and span elements to place converted units into
+const convertedUnits: HTMLDivElement = document.createElement('div');
+const heading: HTMLHeadingElement = document.createElement('h3');
 
+const cmSpan: HTMLSpanElement = document.createElement('span');
+const inSpan: HTMLSpanElement = document.createElement('span');
+const ftSpan: HTMLSpanElement = document.createElement('span');
+const miSpan: HTMLSpanElement = document.createElement('span');
+const ydSpan: HTMLSpanElement = document.createElement('span');
+
+// Append the heading and span's to the div element
+convertedUnits.append(heading, cmSpan, inSpan, ftSpan, miSpan, ydSpan);
+
+// Append the div (that contains span and heading elements) to the form's next sibling div element, if it exists
+form.nextElementSibling?.appendChild(convertedUnits) as HTMLDivElement;
+
+// Append the submit button to form
 form.appendChild(submitBtn);
+
+// Creating an event listener for when the form is submitted
 form.addEventListener('submit', e => {
     e.preventDefault();
     
+    // Get the input value in meters
     const meters: number = metersInput.valueAsNumber;
-    console.log(meters);
 
-    const centimeters: number = meters * 100;
-    const cmSpan: HTMLSpanElement = document.createElement('span');
-    cmSpan.textContent = String(centimeters);
+    // Print the meter value in the heading
+    heading.textContent = `${meters}m converted to:`
 
-    const inches: number = meters * 39.37;
-    const inSpan: HTMLSpanElement = document.createElement('span');
-    inSpan.textContent = String(inches);
-    
-    const feet: number = meters * 3.281;
-    const ftSpan: HTMLSpanElement = document.createElement('span');
-    ftSpan.textContent = String(feet);
-    
-    const miles: number = meters / 1609;
-    const miSpan: HTMLSpanElement = document.createElement('span');
-    miSpan.textContent = String(miles);
-    
-    const yards: number = meters * 1.094;
-    const ydSpan: HTMLSpanElement = document.createElement('span');
-    ydSpan.textContent = String(yards);
+    // Create each unit variable as a string, convert meters into desired units, convert the result to a string with two decimal symbols, assign the result to the span's text content
+    const centimeters: string = (meters * 100).toFixed(2);
+    cmSpan.textContent = `Centimeters:${centimeters}cm`;
 
-    convertedUnits.append(cmSpan, inSpan, ftSpan, miSpan, ydSpan);
-})
+    const inches: string = (meters * 39.37).toFixed(2);
+    inSpan.textContent = `Inches:${inches}in`;
+    
+    const feet: string = (meters * 3.281).toFixed(2);
+    ftSpan.textContent = `Feet:${feet}ft`;
+    
+    const miles: string = (meters / 1609).toFixed(2);
+    miSpan.textContent = `Miles:${miles}mi`;
+    
+    const yards: string = (meters * 1.094).toFixed(2);
+    ydSpan.textContent = `Yards:${yards}yd`;
+
+    form.reset();
+});
