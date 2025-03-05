9431a199d86990c8b93d8f59c88f47f6ef1ee9fa
Paulius19Ka <kallabow2@gmail.com>
2025-03-05 16:24:48 +0200

    Recompiled code

---
+++ Task 3/script.js
@@ -0,0 +1,22 @@
+"use strict";
+const dviratis = {
+    metai: 1999,
+    spalva: 'juoda',
+    marke: 'Mongoose',
+    modelis: 'Colton',
+    kilometrazas: 200
+};
+const naujaMasina = {
+    marke: 'Porsche',
+    modelis: '718 Cayman GT4 RS',
+    metai: 2025,
+    spalva: 'pilka',
+    kilometrazas: 10
+};
+const senaMasina = {
+    marke: 'Porsche',
+    modelis: '911 Carrera',
+    metai: 2025,
+    spalva: 'raudona',
+    kilometrazas: 999999,
+};
